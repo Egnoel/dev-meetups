6a4048147fbb671d9a867ea89f46699033ab4090
Egnoel <70013986+Egnoel@users.noreply.github.com>
2025-11-22 11:52:40 +0100

    fixed issues pt2

--- components/DynamicPage.tsx
+++ components/DynamicPage.tsx
@@ -1,15 +1,14 @@
 import { IEvent } from '@/database';
 import { cacheLife } from 'next/cache';
 import EventCard from '@/components/EventCard';
-import { events } from '@/lib/constants';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const DynamicPage = async () => {
   'use cache';
   cacheLife('hours');
-  //  const response = await fetch(`${BASE_URL}/api/events`);
-  //  const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <ul className="events">
       {events &&
